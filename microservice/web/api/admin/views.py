d14efce2ab12d407fe900a70d653a4f62678ceab
Dallinchi <herooopr@gmail.com>
2024-06-01 08:16:58 +0300

    code review

--- microservice/web/api/admin/views.py
+++ microservice/web/api/admin/views.py
@@ -27,15 +27,3 @@ async def read_user(
     
     update_user =  await crud.user.update_user(db, user_id=user_id, user_update=user)
     return update_user
-
-
-# @router.get("/{user_id:int}")
-# async def read_user(
-#     user_id: int,
-#     current_user: Annotated[PrivateUser, Depends(crud.user.get_current_user)],
-#     db: AsyncSession = Depends(get_db_session),
-# ):
-#     db_user = await crud.user.get_user_by_id(db, user_id=user_id)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return db_user
